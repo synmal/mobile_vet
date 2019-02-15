457b9e1bca39fda294c86fd83d7cf4a0f9d1160d
synmal <malcolmjeremiahrajeli@gmail.com>
2019-02-15 17:37:16 +0800

    Setup user detail and test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,14 +3,26 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   context 'sign up with same email' do
     it 'should give error' do
-        user1 = User.create!(email: 'test@example.com')
-        expect(User.create!(email: 'test@example.com')).to raise_error
+        user1 = User.create!(email: 'test@example.com', name: 'John Smith', address: 'test')
+        expect{ User.create!(email: 'test@example.com', name: 'John Smith', address: 'test') }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+  end
+
+  context 'sign up with empty fields' do
+    it 'should have name' do
+      expect{ User.create!(email: '1@example.com', address: 'test') }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+    it 'should have email' do
+      expect{ User.create!(name: 'John Smith', address: 'test') }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+    it 'should have address' do
+      expect{ User.create!(name: 'John Smith', email: '1@example.com') }.to raise_error(ActiveRecord::RecordInvalid)
     end
   end
 
   context 'should have default role = 1' do
     it 'should have role = 1' do
-      user = User.create!
+      user = User.create!(email: 'test@example.com', name: 'John Smith', address: 'test')
       expect(user.roles).to eq 1
     end
   end
