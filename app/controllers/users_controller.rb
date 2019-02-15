ab17ab416f5644e6764bed85e106a37f88dbdf38
synmal <malcolmjeremiahrajeli@gmail.com>
2019-02-15 18:34:06 +0800

    Added New user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,17 @@
 class UsersController < ApplicationController
-  
+  def new
+    @user = User.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :name, :address, presence: true
+  enum roles: [:user, :doctor, :admin]
+  has_secure_password
 end
