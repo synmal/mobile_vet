277ecc14cb82c54136a193c881ffdd4eb2325bda
synmal <malcolmjeremiahrajeli@gmail.com>
2019-02-15 17:12:23 +0800

    Added rspec user model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'sign up with same email' do
+    it 'should give error' do
+        user1 = User.create!(email: 'test@example.com')
+        expect(User.create!(email: 'test@example.com')).to raise_error
+    end
+  end
+
+  context 'should have default role = 1' do
+    it 'should have role = 1' do
+      user = User.create!
+      expect(user.roles).to eq 1
+    end
+  end
 end
