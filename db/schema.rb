2b8c04ac593b9770489017060122e39f7d5f9d40
synmal <malcolmjeremiahrajeli@gmail.com>
2019-02-16 20:47:03 +0800

    User phone num now encrypted

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_16_114040) do
+ActiveRecord::Schema.define(version: 2019_02_16_124149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2019_02_16_114040) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "encrypted_phone"
+    t.string "encrypted_phone_iv"
   end
 
   add_foreign_key "appointments", "pets"
