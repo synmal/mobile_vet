370d4182ca376a67f569cf90794f063d9b713e20
synmal <malcolmjeremiahrajeli@gmail.com>
2019-02-16 11:34:06 +0800

    Added index view and pet routes. Pet routes nested under user.

---
+++ app/controllers/pets_controller.rb
@@ -0,0 +1,18 @@
+class PetsController < ApplicationController
+  before_action :require_login
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
