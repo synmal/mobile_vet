7f492381802499a096b0b153f3940db58c029096
synmal <malcolmjeremiahrajeli@gmail.com>
2019-02-16 11:24:56 +0800

    Model passed test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_16_020736) do
+ActiveRecord::Schema.define(version: 2019_02_16_022517) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2019_02_16_020736) do
     t.string "pet_avatar"
     t.string "name"
     t.integer "age"
-    t.string "type"
+    t.string "pet_type"
     t.string "breed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -1,24 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Pet, type: :model do
+
   context 'When creating new pet' do
     it 'returns error on empty name' do
-      user = User.first
-      pet = user.pets.new(type: 'cat')
-      expect{ pet.save }.to raise_error(ActiveRecord::RecordInvalid)
+      expect{ Pet.create!(pet_type: 'cat') }.to raise_error(ActiveRecord::RecordInvalid)
     end
 
     it 'returns error on empty type' do
-      user = User.first
-      pet = user.pets.new(name: 'jojo')
-      expect{ pet.save }.to raise_error(ActiveRecord::RecordInvalid)
+      expect{ Pet.create!(name: 'jojo') }.to raise_error(ActiveRecord::RecordInvalid)
     end
 
     it 'should have owner id on create' do
-      user = User.first
-      pet = user.pets.new(name: 'jojo', type: 'cat')
-      pet.save
-      expect(pet.user).to eq(user)
+      pet = build(:pet)
+      expect(pet.user).to_not eq(nil)
     end
   end
 end
