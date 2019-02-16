fdb766073be8c85135a9fbd4fa6e501d3364ad1b
synmal <malcolmjeremiahrajeli@gmail.com>
2019-02-16 14:49:18 +0800

    User now can create appointment

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,42 @@
 class AppointmentsController < ApplicationController
+  before_action :set_user
+  before_action :require_login
+
+  def new
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def create
+    appointment = @user.appointments.new(appointment_params)
+    if appointment.save
+      redirect_to root_path
+      flash[:success] = 'Appointment successfully created.'
+    else
+      redirect_to new_user_appointment_path(@user.id)
+      flash[:error] = appointment.errors.full_messages[0]
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def appointment_params
+    params.require(:appointment).permit(:pet_id, :description, :appointment_date, :location)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users do
     resources :pets, except: [:show]
+    resources :appointments
   end
 
   resources :sessions, only: [:new, :create, :destroy]
