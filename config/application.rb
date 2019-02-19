277461db663aa95a70a8ca99c6a59416ed24d9fb
synmal <malcolmjeremiahrajeli@gmail.com>
2019-02-19 17:28:17 +0800

    Configure activejob queue adapter to use sidekiq

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,6 @@ module MobileVet
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.time_zone = 'Asia/Kuala_Lumpur'
+    config.active_job.queue_adapter = :sidekiq
   end
 end
