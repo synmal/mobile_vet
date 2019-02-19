4e5bd6e9aa0a71231adc4a094b8dc615c36702c0
synmal <malcolmjeremiahrajeli@gmail.com>
2019-02-19 17:52:47 +0800

    Addedd routs for sidekiq UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
+  
   resources :users do
     resources :pets, except: [:show]
     resources :appointments, shallow: true do
